668758110dbc163505f8cc76ea87f330bd928760
Fabian Rinaldi <code.optica@gmail.com>
2017-04-23 14:36:55 +0800

    adding new spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+    before do
+      @user =  User.create(email: 'abc@example.com', password: 'abcdef', password_confirmation: 'abcdef', first_name: 'fabz', last_name: 'rinaldi')
+    end
+    it "can be created" do
+      expect(@user).to be_valid
+    end
+
+    it "must have first name and last name to be valid" do
+      @user.first_name = nil
+      @user.last_name = nil
+      expect(@user).not_to be_valid
+    end
+  end
 end
