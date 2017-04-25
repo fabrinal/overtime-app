9c021755af137b7eb7eeaa05e395b0453e8f669e
Fabian Rinaldi <code.optica@gmail.com>
2017-04-25 15:36:48 +0800

    add post index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
+ @user = User.create(
+        email: 'test@hobedio.com', 
+        password: 'testing1234', 
+        password_confirmation: 'testing1234', 
+        first_name: 'fabz', 
+        last_name: 'rinaldi')
+
+puts "add 1 user"
+
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale")
+  Post.create(date: Date.today, rationale: "#{post} rationale", user_id: @user.id)
 end
 
 puts "create 100 sample"
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,49 +1,58 @@
 require 'rails_helper'
 
   describe 'navigate' do
+    before do
+      @user = User.create(
+        email: 'abc@example.com', 
+        password: 'abcdef', 
+        password_confirmation: 'abcdef', 
+        first_name: 'fabz', 
+        last_name: 'rinaldi')
+      login_as(@user, :scope => :user)
+    end
     describe 'index' do
       before do
-        user = User.create(email: 'abc@example.com', password: 'abcdef', password_confirmation: 'abcdef', first_name: 'fabz', last_name: 'rinaldi')
-        login_as(user, :scope => :user)
-      end
-
-      it 'can be reached successfully' do
         visit posts_path
+      end
+      
+      it 'can be reached successfully' do
         expect(page.status_code).to eq(200)
       end
       it 'has a title of Posts' do
-        visit posts_path
         expect(page).to have_content(/Posts/)
       end
-  end
 
-  describe 'creation' do 
-
-    before do
-      user = User.create(email: 'abc@example.com', password: 'abcdef', password_confirmation: 'abcdef', first_name: 'fabz', last_name: 'rinaldi')
-      login_as(user, :scope => :user)
-      visit new_post_path
+      it 'has a list of posts' do
+        post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+        post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
+        visit posts_path
+        expect(page).to have_content(/Post1|Post2/)
+      end
     end
-    
-    it 'has a new form that can be reach' do
-      expect(page.status_code).to eq(200)
-    end
-
-    it 'can be create from new form page' do
-      fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: 'Some Rationale'
-
-      click_on "Save"
-      expect(page).to have_content("Some Rationale")
-    end
-
-    it 'will have association with current user' do
-      fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: 'User Association'
-      click_on "Save"
+    describe 'creation' do 
+      before do
+        visit new_post_path
+      end
       
-      expect(User.last.posts.last.rationale).to eq("User Association")
+      it 'has a new form that can be reach' do
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'can be create from new form page' do
+        fill_in 'post[date]', with: Date.today
+        fill_in 'post[rationale]', with: 'Some Rationale'
+
+        click_on "Save"
+        expect(page).to have_content("Some Rationale")
+      end
+
+      it 'will have association with current user' do
+        fill_in 'post[date]', with: Date.today
+        fill_in 'post[rationale]', with: 'User Association'
+        click_on "Save"
+        
+        expect(User.last.posts.last.rationale).to eq("User Association")
+      end
     end
-    
+
   end
-end
