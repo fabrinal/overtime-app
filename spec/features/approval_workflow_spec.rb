de2e2a62e4ee3f8bbb07d07d44e7cae00dd71763
Fabian Rinaldi <code.optica@gmail.com>
2017-05-06 20:32:28 +0800

    add test on approval

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -9,16 +9,22 @@ describe 'navigate' do
   describe 'edit' do
     before do
       @post = FactoryGirl.create(:post)
+      visit edit_post_path(@post)
     end
     
-    it 'has a status that can be edited on the form' do
-      visit edit_post_path(@post)
-
+    it 'has a status that can be edited on the form by an admin' do
       choose 'post_status_approved'
-
       click_button 'Save'
-
       expect(@post.reload.status).to eq('approved')
+    end
+    
+    it 'cannot be edited by non admin' do
+      logout(:user)
+       user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      visit edit_post_path(@post)
+      expect(page).not_to have_content('Approved')
     end
 
   end
