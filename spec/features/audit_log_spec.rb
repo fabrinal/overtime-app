179135f1ce3758c23f669d746a9cb1d0e515267f
Fabian Rinaldi <code.optica@gmail.com>
2017-06-03 20:42:31 +0800

    add initial view codes

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe AuditLog, type: :model do 
+describe 'AuditLog Feature' do
+
+  describe 'index' do
+    before do
+      admin_user = FactoryGirl.create(:admin_user)
+      login_as(admin_user, scope: :user)
+      FactoryGirl.create(:audit_log)
+    end
+    
+    it 'has an index page that can be reached' do
+      visit audit_logs_path
+      expect(page.status_code).to eq(200)
+    end
+    it 'renders audit log content' do
+      visit audit_logs_path
+      expect(page).to have_content(/fabz/)
+    end
+    
+  end
+      
 end
