87cd3ea1674d3e2ca49ee2dfac9b5abbd41a3ee9
Fabian Rinaldi <code.optica@gmail.com>
2017-06-04 16:22:07 +0800

    integrate email notification

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'mailer@hobedio.com'
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local = true
@@ -15,11 +15,14 @@ Rails.application.configure do
 
     config.cache_store = :null_store
     end
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_caching = false
+  
+  
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
   config.assets.debug = true
   config.assets.quiet = true
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
 end
