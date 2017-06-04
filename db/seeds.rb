b2ae030395afeba795129f186b858fd4494479ba
Fabian Rinaldi <code.optica@gmail.com>
2017-06-04 17:08:12 +0800

    add nil guard

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,15 @@ AdminUser.create(
         first_name: 'Admin', 
         last_name: 'rinaldi')
 
-100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt", user_id: @user.id, overtime_request: 2.5)
-end
-
-puts "create 100 sample"
 
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
+
+100.times do |post|
+  Post.create(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt", user_id: @user.id, overtime_request: 2.5)
+end
+
+puts "create 100 sample"
